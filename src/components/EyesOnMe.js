40f3c03efc5c0306030e11b16419414b237e073a
Yoshi <yoshimejia00@gmail.com>
2021-11-01 18:08:22 -0400

    commit tree 680e19edc73ff7a4fdfed995edbae021570cc8a1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 export default class EyesOnMe extends Component {
 
     render(){
-        return <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')} ></button>
+        return <button onFocus={() => console.log('Good!')} 
+        onBlur={() => console.log('Hey! Eyes on me!')} >
+        </button>
     }
 } 
